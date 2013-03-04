279c969763471ef3ee9f1001baf4575b5d83df96
Cody Hanson <cody.hanson@flukenetworks.com>
2013-03-04 16:51:31 -0700

    finished level 1. on to level 2

--- level1.py
+++ level1.py
@@ -5,12 +5,49 @@
 #O -> Q
 #E -> G
 
-stringdata = 
-""" g fmnc wms bgblr rpylqjyrc gr zw fylb.
+from string import maketrans
+
+
+stringdata = """ g fmnc wms bgblr rpylqjyrc gr zw fylb.
 rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw 
 fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. 
 sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
 
+mappings = {}
+
+def mapFunc(inCh):
+    if (inCh in mappings):
+        return mappings[inCh]
+    else:
+        return inCh
+
+#it would seem that each letter needs to be
+#shifted by two (mod 26?)
+for i in xrange(26):
+    mappings[chr(ord('a')+i)] =  chr(ord('a')+ ((i + 2) % 26))
 
 
+print ''.join(map(mapFunc ,stringdata))
 
+#apparently the preferred way is 'maketrans'
+intab = mappings.keys()
+intab = ''.join(intab)
+outtab = ''
+for key in intab:
+    outtab = outtab + mappings[key]
+
+
+translation = maketrans(intab,outtab)
+
+print 'USING MAKETRANS'
+print stringdata.translate(translation)
+
+###############################################
+#Part 2!
+url = "http://www.pythonchallenge.com/pc/def/map.html"
+
+print ''.join(map(mapFunc ,url))
+
+T1 = url.translate(translation)
+T2 = T1.translate(translation)
+print T2

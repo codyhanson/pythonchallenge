5592edf2cec736965d134631adb90ce51dfbbc39
Cody Hanson <cody.hanson@flukenetworks.com>
2013-03-04 18:52:12 -0700

    level 4 down.

--- level4.py
+++ level4.py
@@ -1,6 +1,34 @@
 #! /usr/bin/env python
 #python challenge, level 4!
 
-url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php'
+import re
+import urllib2
+
+baseurl = 'http://www.pythonchallenge.com/pc/def/linkedlist.php'
+
+nothingNumber = re.compile(r"(\d+)$")
+
+url = baseurl + '?nothing=0'
+
+nothingsSeen = {}
 
 #follow the ?nothing 's
+for i in xrange(400):
+    openedUrl = urllib2.urlopen(url)
+    page = openedUrl.read()
+    m = re.search(nothingNumber,page)
+    if (m == None):
+        print page
+        break
+
+    nothingNum = m.group(1)
+
+    if (nothingNum in nothingsSeen):
+        print "Seen {0} before".format(nothingNum)
+    else:
+        print i,nothingNum
+        
+    nothingsSeen[nothingNum] = True
+    #next url
+    url = baseurl + '?nothing=' + nothingNum 
+
---
+++ level5.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+#python challenge, level 5
+
+url = 'http://www.pythonchallenge.com/pc/def/peak.html'

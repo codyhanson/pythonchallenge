30ecdd6d6f379afd1c0adbbae8859728b49ff466
Cody Hanson <cody.hanson@flukenetworks.com>
2013-03-07 16:44:29 -0700

    still stumped on 5

--- level5.py
+++ level5.py
@@ -6,6 +6,7 @@ import urllib2
 
 
 #pronounce it: 'peak hell'. 
+#picture is 'green hill'?
 # font color: #c0c0ff. pronounce that too?
 url = 'http://www.pythonchallenge.com/pc/def/peak.html'
 urlBanner = 'http://www.pythonchallenge.com/pc/def/banner.p'
@@ -13,4 +14,16 @@ urlBanner = 'http://www.pythonchallenge.com/pc/def/banner.p'
 urlBannerHdl = urllib2.urlopen(urlBanner)
 banner = urlBannerHdl.read()
 
+output = ''
 #try to remove all the p's?
+for char in banner:
+    if (char != 'p'):
+        output = output + char
+
+print output
+
+
+#lines =  banner.split('\n')
+#lines.sort()
+#for line in lines:
+#    print line

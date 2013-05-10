b8989e78c828886eafea2d7d44147cbc218d07dc
Cody Hanson <cody.hanson@flukenetworks.com>
2013-05-10 13:02:43 -0600

    Got level 5. That was pretty interesting.

--- level5.py
+++ level5.py
@@ -3,27 +3,29 @@
 
 import re
 import urllib2
+import pickle
+import pprint
+import sys
 
 
 #pronounce it: 'peak hell'. 
 #picture is 'green hill'?
-# font color: #c0c0ff. pronounce that too?
+#Pickle!
 url = 'http://www.pythonchallenge.com/pc/def/peak.html'
 urlBanner = 'http://www.pythonchallenge.com/pc/def/banner.p'
 
 urlBannerHdl = urllib2.urlopen(urlBanner)
 banner = urlBannerHdl.read()
 
-output = ''
-#try to remove all the p's?
-for char in banner:
-    if (char != 'p'):
-        output = output + char
+#unpickle the banner.p file
+bannerobj = pickle.loads(banner)
 
-print output
+#my hypothesis is that the ' ' and the '#' make some kind of ascii art.
+#when printed in the proper way
+for item in bannerobj:
+    for subitem in item:
+        sys.stdout.write(subitem[0] * subitem[1])
+    sys.stdout.write('\n')
 
-
-#lines =  banner.split('\n')
-#lines.sort()
-#for line in lines:
-#    print line
+#POST MORTEM: That was sweet as hell.
+#output stored in level5-output.txt
---
+++ level6.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+#Python Challenge, Level 6
+
+import urllib2
+
+url = 'http://www.pythonchallenge.com/pc/def/channel.html'
+imgurl = 'http://www.pythonchallenge.com/pc/def/channel.jpg'
+
+#hints: Channel, zip, now there are pairs
+
+

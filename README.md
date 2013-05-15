a1fd59067732dd2b530037d7ddf007ca853f5e93
Cody Hanson <cody.hanson@flukenetworks.com>
2013-05-15 07:40:12 -0600

    playing around with Zip and PIL.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ pythonchallenge
 I am taking the python challenge, to better learn how to wield Python.
 
 http://www.pythonchallenge.com/
+
+
+*Warning: Spoilers in this repo!*
+---------------------------------
+Don't read too deep if you want to take the challenge yourself.
--- level6.py
+++ level6.py
@@ -2,10 +2,48 @@
 #Python Challenge, Level 6
 
 import urllib2
+import pickle
+import re
+import string
+import sys
+import StringIO
+from PIL import Image
 
 url = 'http://www.pythonchallenge.com/pc/def/channel.html'
-imgurl = 'http://www.pythonchallenge.com/pc/def/channel.jpg'
+urlImg = 'http://www.pythonchallenge.com/pc/def/channel.jpg'
 
-#hints: Channel, zip, now there are pairs
+#hints: Channel, <-- zip (means unzip?), now there are pairs
 
+
+urlImgHdl = urllib2.urlopen(urlImg)
+imageData = urlImgHdl.read()
+
+pairs = zip(*[imageData]*2)
+
+bands = zip(*pairs)
+print bands
+
+img = Image.merge("LA",bands)
+
+img.save("img.jpg")
+
+#turn bytes into hex string
+#imageHex = ":".join("{0:x}".format(ord(c)) for c in imageData)
+imageHex = "".join("{0:x}".format(ord(c)) for c in imageData)
+
+#print imageHex
+
+for i in xrange(len(imageHex)):
+    if imageHex[i:i+4] == 'fffe':
+        pass
+    #print imageHex[i:i+60].decode("hex")
+
+
+
+#sfp = StringIO.StringIO(imageData)
+#im = Image.open(sfp)
+
+#print im
+
+#z1,z2,z3 = zip(*imageData)
 
